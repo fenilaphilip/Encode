12ec7a8e0d6843a627e6729d4f0738189f048a1b
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-06-28 15:21:05 +0200

    Clearing git add-ons

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -1,17 +1,9 @@
 export default function Input() {
   return (
-<<<<<<< HEAD
     <form className="user-input">
       <label>Enter Text</label>
       <textarea></textarea>
       <button>Submit</button>
     </form>
-=======
-    <div className="user-input">
-      <label>Enter Text</label>
-      <textarea></textarea>
-      <button>Submit</button>
-    </div>
->>>>>>> 1789a51ef96d61cd0b25226969ce48464dd543a1
   );
 }
