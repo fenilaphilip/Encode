1789a51ef96d61cd0b25226969ce48464dd543a1
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-06-28 11:16:28 +0200

    Adding new Input component

---
+++ src/components/UI/Input.tsx
@@ -0,0 +1,9 @@
+export default function Input() {
+  return (
+    <div className="user-input">
+      <label>Enter Text</label>
+      <textarea></textarea>
+      <button>Submit</button>
+    </div>
+  );
+}
