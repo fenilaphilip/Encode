cc35c651cbb88516b7f8742446b69d6e7fa876c9
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-07-10 15:28:50 +0200

    Basic styling added

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -2,6 +2,13 @@ const Button: React.FC<{ type: "submit"; children: string }> = ({
   type,
   children,
 }) => {
-  return <button type={type}>{children}</button>;
+  return (
+    <button
+      type={type}
+      className="text-white bg-gray-800 hover:bg-gray-400 hover:text-black focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 m-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+    >
+      {children}
+    </button>
+  );
 };
 export default Button;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,11 +3,13 @@ import { Items } from "../../data.ts";
 
 export default function Navbar() {
   return (
-    <ul className=" bg-gray-900 text-white relative flex h-16 items-center justify-between mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <h1>ENCODER</h1>
-      {Items.map((item) => (
-        <NavItem key={item.name} navItem={item} />
-      ))}
+    <ul className="bg-gray-900 text-white relative h-20 items-center mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <h1 className="sm:text-2xl lg:text-4xl">ENCODER</h1>
+      <div className="flex justify-end">
+        {Items.map((item) => (
+          <NavItem key={item.name} navItem={item} />
+        ))}
+      </div>
     </ul>
   );
 }
