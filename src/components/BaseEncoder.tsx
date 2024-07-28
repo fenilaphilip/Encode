24ba04eadf1d3fac4504a1b9558db7cd8323c43b
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-07-28 14:24:22 +0200

    Style changed

--- src/components/BaseEncoder.tsx
+++ src/components/BaseEncoder.tsx
@@ -28,33 +28,27 @@ const BaseEncoder: React.FC = () => {
     <>
       <div className="mx-auto max-w-7xl px-2 py-4 sm:px-6 lg:px-8 bg-slate-200">
         <h1 className="flex justify-center text-3xl p-4 m-3">Base 64 Encode</h1>
-        <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-1 ">
-          <div className="lg:col-span-2 sm:col-span-full">
-            <Input btnName="Convert to Base 64" inputConsumer={inputConsumer} />
-            {result && <Result result={result} userInput={inputText} />}
-            <Button type="button" onClick={handleViewButtonClick}>
-              {displayHistory ? "Hide " : "View "} Old Encoded Texts
-            </Button>
-            {displayHistory && (
-              <History
-                store="BaseEncoder"
-                input={inputText}
-                showAgain={handleShowAgainText}
-              />
-            )}
-          </div>
 
-          <div className="lg:col-span-1  sm:col-span-full">
-            <InfoBox heading="What is Base 64 Encoding?">
-              Base64 is a group of binary-to-text encoding schemes that
-              transforms binary data into a sequence of printable characters,
-              limited to a set of 64 unique characters.Base64 is also widely
-              used for sending e-mail attachments, because SMTP – in its
-              original form – was designed to transport 7-bit ASCII characters
-              only.
-            </InfoBox>
-          </div>
-        </div>
+        <Input btnName="Convert to Base 64" inputConsumer={inputConsumer} />
+        {result && <Result result={result} userInput={inputText} />}
+        <Button type="button" onClick={handleViewButtonClick}>
+          {displayHistory ? "Hide " : "View "} Old Encoded Texts
+        </Button>
+        {displayHistory && (
+          <History
+            store="BaseEncoder"
+            input={inputText}
+            showAgain={handleShowAgainText}
+          />
+        )}
+
+        <InfoBox heading="What is Base 64 Encoding?">
+          Base64 is a group of binary-to-text encoding schemes that transforms
+          binary data into a sequence of printable characters, limited to a set
+          of 64 unique characters.Base64 is also widely used for sending e-mail
+          attachments, because SMTP – in its original form – was designed to
+          transport 7-bit ASCII characters only.
+        </InfoBox>
       </div>
     </>
   );
--- src/components/QrEncoder.tsx
+++ src/components/QrEncoder.tsx
@@ -25,38 +25,33 @@ const QrEncoder: React.FC = () => {
     <>
       <div className="mx-auto max-w-7xl px-2 py-4 sm:px-6 lg:px-8 bg-slate-200">
         <h1 className="flex justify-center text-3xl p-4 m-3">QR Code</h1>
-        <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-1">
-          <div className="lg:col-span-2 sm:col-span-full">
-            <Input btnName="Generate QR Code" inputConsumer={inputConsumer} />
-            {result && (
-              <>
-                <h2 className="px-4">QR Code for {userInput}</h2>
-                <div className="flex m-2 justify-center">
-                  <QRCode value={result} />
-                </div>
-              </>
-            )}
-            <Button type="button" onClick={handleViewButtonClick}>
-              {displayHistory ? "Hide " : "View "} Old Encoded Texts
-            </Button>
-            {displayHistory && (
-              <History
-                store="QREncoder"
-                input={userInput}
-                showAgain={handleShowAgainText}
-              />
-            )}
-          </div>
-          <div className="lg:col-span-1  sm:col-span-full">
-            <InfoBox heading="What is QR Encoding?">
-              A QR code (quick-response code) is a type of two-dimensional
-              matrix barcode and also a machine-readable optical image that
-              contains information specific to the labelled item, the QR code
-              contains the data for a locator, an identifier, and for
-              web-tracking.
-            </InfoBox>
-          </div>
-        </div>
+
+        <Input btnName="Generate QR Code" inputConsumer={inputConsumer} />
+        {result && (
+          <>
+            <h2 className="px-4">QR Code for {userInput}</h2>
+            <div className="flex m-2 justify-center">
+              <QRCode value={result} />
+            </div>
+          </>
+        )}
+        <Button type="button" onClick={handleViewButtonClick}>
+          {displayHistory ? "Hide " : "View "} Old Encoded Texts
+        </Button>
+        {displayHistory && (
+          <History
+            store="QREncoder"
+            input={userInput}
+            showAgain={handleShowAgainText}
+          />
+        )}
+
+        <InfoBox heading="What is QR Encoding?">
+          A QR code (quick-response code) is a type of two-dimensional matrix
+          barcode and also a machine-readable optical image that contains
+          information specific to the labelled item, the QR code contains the
+          data for a locator, an identifier, and for web-tracking.
+        </InfoBox>
       </div>
     </>
   );
